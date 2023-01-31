36a7c15024acafd4350b8d48ac5a324cb70e765d
baguilarislas <baguilarislas@hawk.iit.edu>
2023-01-31 10:39:16 -0600

    Created edges with weights

--- partA.py
+++ partA.py
@@ -17,3 +17,25 @@ args = parser.parse_args()
 # Create a list of vertices
 
 vertices = [str(i) for i in range(args.vertices)]
+
+# Create a list of edges and their weights
+
+edges = []
+
+for i in range(args.vertices):
+
+    for j in range(i+1, args.vertices):
+
+        if args.connected and i == 0 and j == 1:
+
+            weight = round(random.uniform(0, 1), 4)
+
+        elif args.connected and i != 0 and j != 1:
+
+            weight = round(random.uniform(0, 1), 4)
+
+        else:
+
+            weight = round(random.uniform(0, 1), 4)
+
+        edges.append((i, j, weight))

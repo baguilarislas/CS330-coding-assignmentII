02cee674b99c9a1e0c5bc02742b32ca998c36617
baguilarislas <baguilarislas@hawk.iit.edu>
2023-02-01 09:12:53 -0600

    Print edges and weights and formatted

--- partA.py
+++ partA.py
@@ -1,15 +1,11 @@
 import argparse
-
 import random
 
 
 
 parser = argparse.ArgumentParser(description='Generate a weighted graph')
-
 parser.add_argument('--vertices', type=int, default=random.randint(2, 10), help='Number of vertices in the graph')
-
 parser.add_argument('--connected', action='store_true', help='Indicates if the graph should be connected')
-
 args = parser.parse_args()
 
 
@@ -25,17 +21,17 @@ edges = []
 for i in range(args.vertices):
 
     for j in range(i+1, args.vertices):
-
         if args.connected and i == 0 and j == 1:
-
             weight = round(random.uniform(0, 1), 4)
-
+            
         elif args.connected and i != 0 and j != 1:
-
             weight = round(random.uniform(0, 1), 4)
 
         else:
-
             weight = round(random.uniform(0, 1), 4)
-
         edges.append((i, j, weight))
+
+
+# Print the edges and their weights
+for edge in edges:
+    print("{} - {} : {}".format(vertices[edge[0]], vertices[edge[1]], edge[2]))
